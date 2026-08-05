75aa3d74a4d910e00cb410e79ca2144558207aa8
Juan Miguel Recondo <134484809+jmrec@users.noreply.github.com>
2026-08-05 14:13:41 +0800

    fix(devcontainer): fix setup.sh to use terraform.example.tfvars instead of terraform.tfvars.example

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp -n .env.example .env
-cp -n terraform/environments/dev/terraform.tfvars.example terraform/environments/dev/terraform.tfvars
+cp -n terraform/environments/dev/terraform.example.tfvars terraform/environments/dev/terraform.tfvars
 npm install
